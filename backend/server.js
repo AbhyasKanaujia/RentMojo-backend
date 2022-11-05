706d6c2f073946fb71591463bac600f4b6f29e17
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-05 22:36:20 +0530

    add first route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api/status", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
 app.listen(
   PORT,
   console.log(

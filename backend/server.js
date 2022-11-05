a5994d7228ca60d2766502001d11b61d257e7305
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-05 22:33:19 +0530

    setup basic express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Hello RentMojo <3");
+const express = require("express");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`
+  )
+);

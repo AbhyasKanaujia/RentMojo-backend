cbd70ea1384cebbe7ca84f8550776d2c55472359
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-05 23:07:17 +0530

    add express async handler

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,41 +1,43 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Get all products
 // @route   GET /api/products/
 // @access  Public
-const getProducts = (req, res) => {
+const getProducts = asynchandler(async (req, res) => {
   res.status(200).json({ message: "Get all products" });
-};
+});
 
 // @desc    Create a product
 // @route   POST /api/products/
 // @access  Private
-const createProduct = (req, res) => {
+const createProduct = asynchandler(async (req, res) => {
   res.status(201).json({ message: "Craete a product" });
-};
+});
 
 // @desc    Get a product
 // @route   GET /api/products/:id
 // @access  Public
-const getProduct = (req, res) => {
+const getProduct = asynchandler(async (req, res) => {
   res.status(200).json({ message: `Get a product with id ${req.params.id}` });
-};
+});
 
 // @desc    Update a product
 // @route   PUT /api/products/
 // @access  Private
-const updateProdcut = (req, res) => {
+const updateProdcut = asynchandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Update a product with id ${req.params.id}` });
-};
+});
 
 // @desc    Delete a product
 // @route   DELETE /api/products/
 // @access  Private
-const deleteProduct = (req, res) => {
+const deleteProduct = asynchandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Delete a product with id ${req.params.id}` });
-};
+});
 
 module.exports = {
   getProducts,

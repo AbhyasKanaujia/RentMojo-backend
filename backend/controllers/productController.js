6a8715b1b842e22d58c115d5c4fbb1403e49c2d2
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-05 23:06:13 +0530

    Add documenting comments for product controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,21 +1,36 @@
+// @desc    Get all products
+// @route   GET /api/products/
+// @access  Public
 const getProducts = (req, res) => {
   res.status(200).json({ message: "Get all products" });
 };
 
+// @desc    Create a product
+// @route   POST /api/products/
+// @access  Private
 const createProduct = (req, res) => {
   res.status(201).json({ message: "Craete a product" });
 };
 
+// @desc    Get a product
+// @route   GET /api/products/:id
+// @access  Public
 const getProduct = (req, res) => {
   res.status(200).json({ message: `Get a product with id ${req.params.id}` });
 };
 
+// @desc    Update a product
+// @route   PUT /api/products/
+// @access  Private
 const updateProdcut = (req, res) => {
   res
     .status(200)
     .json({ message: `Update a product with id ${req.params.id}` });
 };
 
+// @desc    Delete a product
+// @route   DELETE /api/products/
+// @access  Private
 const deleteProduct = (req, res) => {
   res
     .status(200)

a87c3b8456eaaf8cfc0ac22ced111f6d0b72b327
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-07 20:23:52 +0530

    implement get user cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require("express-async-handler");
+const Cart = require("../models/userModel.js");
 
 // @desc    Get user cart
 // @route   GET /api/carts/:userId
 // @access  Private
 const getUserCart = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get cart for user ${req.params.userId}` });
+  const userCart = await Cart.find({ user: req.params.userId });
+
+  res.status(200).json(userCart);
 });
 
 // @desc    Update user cart

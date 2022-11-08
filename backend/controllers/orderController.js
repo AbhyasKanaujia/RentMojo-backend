603423b7937fd490012bd9425aa63c01b857b4fb
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-08 19:53:13 +0530

    implement create an order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Order = require("../models/orderModel.js");
+const Cart = require("../models/cartModel.js");
 
 // @desc    Get all orders
 // @route   GET /api/orders/
@@ -10,13 +11,59 @@ const getOrders = asyncHandler(async (req, res) => {
   res.status(200).json(orders);
 });
 
-// @desc    Create orders for user using cart
+// @desc    Create order using cart
 // @route   POST /api/orders/
 // @access  Private
 const createOrder = asyncHandler(async (req, res) => {
-  res.status(201).json({
-    message: `Create orders for ${req.user.name} using cart with id ${req.user.cartId}`,
+  const cart = await Cart.findById(req.user.cartId);
+
+  if (cart.products.length === 0) {
+    res.status(400);
+
+    throw new Error("Cart is empty");
+  }
+
+  if (
+    !req.body.address ||
+    !req.body.city ||
+    !req.body.postalCode ||
+    !req.body.country ||
+    !req.body.paymentMethod ||
+    !req.body.shippingPrice ||
+    !req.body.totalPrice ||
+    !req.body.isPaid ||
+    !req.body.isDelivered
+  ) {
+    res.status(400);
+
+    throw new Error("Please provide all fields");
+  }
+
+  const newOrder = await Order.create({
+    user: req.user._id,
+    orderItems: cart.products,
+    shippingAddress: {
+      address: req.body.address,
+      city: req.body.city,
+      postalCode: req.body.postalCode,
+      country: req.body.country,
+    },
+    paymentMethod: req.body.paymentMethod,
+    paymentResult: {
+      id: req.body.paymentId,
+      status: req.body.paymentStatus,
+      update_time: req.body.parymentUpdateTime,
+      email_address: req.body.paymentEmail,
+    },
+    shippingPrice: req.body.shippingPrice,
+    totalPrice: req.body.totalPrice,
+    isPaid: req.body.isPaid,
+    paidAt: req.body.paidAt,
+    isDelivered: req.body.isDelivered,
+    deliveredAt: req.body.deliveredAt,
   });
+
+  res.status(201).json(newOrder);
 });
 
 // @desc    Get an order
